5c366e9e4c215203279906a461a9713c20423d91
Esha Awasthi <eshaawasthi55@gmail.com>
2024-04-23 23:21:35 +0530

    Added ice cream action creator, reducer

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const redux = require("redux");
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+
 // ACTION CREATOR - FUNCTION THAT CREATED AN ACTION (OBJECT WITH PROPERTY TYPE)
 
 function orderCake() {
@@ -18,17 +22,42 @@ function restockedCake(quantity) {
   };
 }
 
-// REDUCERS (prevState, action) => newState
+function orderIceCream(quantity) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: quantity,
+  };
+}
+
+function restockedIceCream(quantity) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: quantity,
+  };
+}
+
 const initialState = {
   numOfCakes: 10,
+  numberOfIceCreams: 10,
 };
 
+// REDUCERS (prevState, action) => newState
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return { ...state, numOfCakes: state.numOfCakes - 1 };
     case CAKE_RESTOCKED:
       return { ...state, numOfCakes: state.numOfCakes + action.payload };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numberOfIceCreams: state.numberOfIceCreams - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numberOfIceCreams: state.numberOfIceCreams + action.payload,
+      };
     default:
       return state;
   }
@@ -43,9 +72,12 @@ const unsubscribe = store.subscribe(() => {
   console.log("Update state", store.getState());
 });
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(restockedCake(5));
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockedCake(5));
+
+store.dispatch(orderIceCream(1));
+store.dispatch(restockedIceCream(1));
 
 // BIND ACTION CREATOR - ALternative way of dispatching an action
 // const bindActionCreator = redux.bindActionCreators;
